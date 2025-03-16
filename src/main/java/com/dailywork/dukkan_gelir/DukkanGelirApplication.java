23c689ac1185905d8a32ba5ce63fa2101fe222c7
gulsemins <gulsemin111@gmail.com>
2025-03-16 15:43:30 +0300

    initial commit

---
+++ src/main/java/com/dailywork/dukkan_gelir/DukkanGelirApplication.java
@@ -0,0 +1,13 @@
+package com.dailywork.dukkan_gelir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukkanGelirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DukkanGelirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailywork/dukkan_gelir/DukkanGelirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailywork.dukkan_gelir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DukkanGelirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
