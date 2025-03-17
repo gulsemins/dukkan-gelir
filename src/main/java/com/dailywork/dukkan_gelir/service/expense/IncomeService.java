bb570acc2a1244880a848f2f12f820a3d319b97c
gulsemins <gulsemin111@gmail.com>
2025-03-17 18:15:34 +0300

    implement income service with create and mapping logic

---
+++ src/main/java/com/dailywork/dukkan_gelir/service/expense/IncomeService.java
@@ -0,0 +1,26 @@
+package com.dailywork.dukkan_gelir.service.expense;
+
+import com.dailywork.dukkan_gelir.dtos.CreateIncomeDto;
+import com.dailywork.dukkan_gelir.dtos.IncomeResponseDto;
+import com.dailywork.dukkan_gelir.entities.IncomeEntity;
+import com.dailywork.dukkan_gelir.mapper.IncomeMapper;
+import com.dailywork.dukkan_gelir.repository.IncomeRepository;
+import jakarta.persistence.Entity;
+import lombok.*;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class IncomeService {
+    private final IncomeRepository incomeRepository;
+    private final IncomeMapper incomeMapper;
+
+    public IncomeResponseDto addIncome(CreateIncomeDto createIncomeDto){
+      IncomeEntity income = incomeMapper.toEntity(createIncomeDto);
+      IncomeEntity savedIncome = incomeRepository.save(income);
+
+      return incomeMapper.toIncomeResponseDto(savedIncome);
+
+    }
+}
